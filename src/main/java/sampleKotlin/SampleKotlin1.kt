cce4c9c410143215bb358d52a0ff298bf4baf3a6
HaenaraShin <hamster12345@gmail.com>
2020-06-07 23:19:54 +0900

    Add Kotlin sample

--- src/main/java/sampleKotlin/SampleKotlin1.kt
+++ src/main/java/sampleKotlin/SampleKotlin1.kt
@@ -3,6 +3,8 @@ package sampleKotlin
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class SampleKotlin1 {
     fun run1() {
@@ -16,5 +18,19 @@ class SampleKotlin1 {
         // let's make it more simple
         Observable.just("Hello RxJava!!")
             .subscribe { println(it) }
+    }
+
+    fun run3() {
+        val o = object : Observer<String>{
+            override fun onComplete() { println("onComplete") }
+            override fun onSubscribe(d: Disposable?) { println("onSubscribe") }
+            override fun onNext(t: String?) { println(t) }
+            override fun onError(e: Throwable?) { }
+        }
+
+        o.onNext("Hello")
+        o.onNext("RxKotlin")
+        o.onNext("Observer with manually.")
+        o.onComplete()
     }
 }
