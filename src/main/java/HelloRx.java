080952b34025d2f6f51242ea966d818db3904389
HaenaraShin <hamster12345@gmail.com>
2020-06-07 23:21:04 +0900

    Add more sample of scan(), reduce(), distinct()

--- src/main/java/HelloRx.java
+++ src/main/java/HelloRx.java
@@ -1,4 +1,5 @@
 //import sample1.Sample2;
+import sample1.Sample1;
 import sample1.Sample3;
 import sample1.Sample4;
 import sample1.Sample5;
@@ -6,7 +7,7 @@ import sampleKotlin.SampleKotlin1;
 
 public class HelloRx {
     public static void main(String[] args) {
-        new Sample5().run2();
+        new Sample5().run4();
     }
 }
 
--- src/main/java/sample1/Sample5.java
+++ src/main/java/sample1/Sample5.java
@@ -6,9 +6,10 @@ import io.reactivex.rxjava3.core.Single;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Sample5 {
-    public void run1(){
+    public void run1() {
         Observable<BigInteger> factorials = Observable
                 .range(2, 100)
                 .scan(BigInteger.ONE, (big, cur) ->
@@ -17,12 +18,63 @@ public class Sample5 {
         factorials.subscribe(System.out::println);
     }
 
-    public void run2(){
+    public void run2() {
         ArrayList<Integer> list = new ArrayList<>();
         Single<ArrayList<Integer>> all = Observable
                 .range(10, 20)
-                .collect(()->list, (l, p)-> l.add(p));
+                .collect(() -> list, (l, p) -> l.add(p));
 
         all.subscribe(System.out::println);
     }
+
+    public void run3() {
+        // scan은 누진계산시에 사용한다.
+        Observable.range(0, 10)
+                .doOnNext(i -> System.out.println("onNext :: " + i))
+                .scan((sum, i) -> sum + i)
+                .subscribe(System.out::println);
+
+        // reduce는 누진계산하되 마지막만 뱉는다.
+        Observable.range(0, 10)
+                .doOnNext(i -> System.out.println("onNext :: " + i))
+                .reduce((sum, i) -> sum + i)
+                .subscribe(System.out::println);
+    }
+
+    public void run4() {
+        // distinct는 이전까지 중복되지 않던 값만 받는다.
+        Observable.create(s -> {
+            Random random = new Random();
+            while (!s.isDisposed()) {
+                s.onNext(random.nextInt(100));
+            }
+        })
+//                .doOnNext(i -> System.out.println("onNext :: " + i))
+                .distinct()
+                .take(10)
+                .subscribe(System.out::println);
+
+        Observable<Foo> o = Observable.create(s -> {
+            s.onNext(new Foo("123", 123));
+            s.onNext(new Foo("123", 456));
+            s.onNext(new Foo("111", 123));
+            s.onNext(new Foo("112", 456));
+            s.onComplete();
+        });
+//                .distinct()
+                o.distinct(f -> f.id)
+                        .subscribe(f -> System.out.println(f.id + " " + f.value));
+    }
+
+
+
+    class Foo {
+        public Foo(String id, int value){
+            this.id = id;
+            this.value = value;
+        }
+        public String id;
+        public int value;
+    }
+
 }
