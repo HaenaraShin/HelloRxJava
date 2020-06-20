4f12ca663a10226525fcd68e67e46627fa5eb44f
HaenaraShin <hamster12345@gmail.com>
2020-06-20 23:32:04 +0900

    Add Kotlin examples

--- src/main/java/HelloRx.java
+++ src/main/java/HelloRx.java
@@ -4,10 +4,14 @@ import sample1.Sample3;
 import sample1.Sample4;
 import sample1.Sample5;
 import sampleKotlin.SampleKotlin1;
+import sampleKotlin.SampleKotlin3;
 
 public class HelloRx {
     public static void main(String[] args) {
-        new Sample5().run5();
+        new SampleKotlin3().run3();
+
+
+
     }
 }
 
--- src/main/java/sampleKotlin/SampleKotlin3.kt
+++ src/main/java/sampleKotlin/SampleKotlin3.kt
@@ -1,6 +1,7 @@
 package sampleKotlin
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class SampleKotlin3 {
@@ -15,6 +16,27 @@ class SampleKotlin3 {
         o.subscribe { println(it) }
         o.subscribe { println(it) }
 
-        try { Thread.sleep(1000) } catch (e :InterruptedException) { }
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+        }
+    }
+
+    fun run2() {
+        Observable.range(2, 8)
+            .flatMap(
+                { Observable.range(1, 9) },
+                { num, i -> "$num x $i = ${num * i}" })
+            .subscribe(System.out::println)
+    }
+
+    fun run3() {
+        Observable.create<String> { s ->
+            s.onNext("Hello")
+            s.onNext("Rx")
+            s.onNext("Hello")
+            s.onNext("Kidsnote")
+            s.onComplete()
+        }.subscribe { hello: String -> println(hello) }
     }
 }
